a4b7d31560f78085424a7b6f633a0d0ccfcaa814
Bezrukov Mikhail <rdbmw@mail.ru>
2017-10-12 13:20:21 +0300

    backup commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// $(function() {
+//   console.log('Im so ready!');
+//   document.querySelector(".banner-gender__title").classList.toggle("banner-gender__title--appeared");
+//   document.querySelector(".banner-gender__description").classList.toggle("banner-gender__description--appeared");
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   document.querySelector(".banner-gender__title").classList.toggle("banner-gender__title--appeared");
+//   document.querySelector(".banner-gender__description").classList.toggle("banner-gender__description--appeared");
+// });
